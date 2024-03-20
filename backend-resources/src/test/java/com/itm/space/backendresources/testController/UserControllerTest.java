97d9f0364dfb9561d28e0842eda41d2265bdd2df
Vadim <cybersnuff@yandex.ru>
2024-03-20 21:48:59 +0400

    commit

---
+++ backend-resources/src/test/java/com/itm/space/backendresources/testController/UserControllerTest.java
@@ -0,0 +1,74 @@
+package com.itm.space.backendresources.testController;
+
+
+import com.itm.space.backendresources.BaseIntegrationTest;
+import com.itm.space.backendresources.api.request.UserRequest;
+import com.itm.space.backendresources.api.response.UserResponse;
+import com.itm.space.backendresources.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import java.util.List;
+import java.util.UUID;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class UserControllerTest extends BaseIntegrationTest {
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    @WithMockUser(username = "testUser", roles = {"MODERATOR"})
+    public void testCreateUser() throws Exception {
+        UserRequest userRequest = new UserRequest(
+                "john_doe",
+                "john.doe@example.com",
+                "securePassword123",
+                "John",
+                "Doe");
+        var request = requestWithContent(post("/api/users"), userRequest);
+        Mockito
+                .doNothing()        // создаем заглушки для метода, который должен ничего не делать
+                .when(userService)     // когда мы юзаем юзерСервис
+                .createUser(any(UserRequest.class));
+        mvc.perform(request)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "testUser", roles = {"MODERATOR"})
+    public void testGetUserById() throws Exception {
+        UUID id = UUID.randomUUID();
+        UserResponse userResponse = new UserResponse("john_doe",
+                "Goshin",
+                "myEmail.com",
+                List.of("USER"),
+                List.of("Manager"));
+        Mockito.when(userService.getUserById(id)).thenReturn(userResponse);
+        mvc.perform(get("/api/users/{id}", id.toString()))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("myEmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Goshin"))
+                .andReturn();
+    }
+
+    @Test
+    @WithMockUser(username = "testUser", roles = {"MODERATOR"})
+    public void testHello() throws Exception {
+        mvc.perform(get("/api/users/hello")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
+
---
+++ backend-resources/src/test/java/com/itm/space/backendresources/testUserService/UserServiceTest.java
@@ -0,0 +1,65 @@
+package com.itm.space.backendresources.testUserService;
+
+import com.itm.space.backendresources.BaseIntegrationTest;
+import com.itm.space.backendresources.api.request.UserRequest;
+import com.itm.space.backendresources.api.response.UserResponse;
+import com.itm.space.backendresources.service.UserServiceImpl;
+import org.junit.jupiter.api.*;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+@SpringBootTest
+public class UserServiceTest extends BaseIntegrationTest {
+
+        @Autowired
+        private UserServiceImpl userService;
+
+        @Autowired
+        private Keycloak keycloak;
+
+        @Test
+        @Order(0)
+        void createUserTest() {
+            UserRequest userRequest = createUserRequest();
+            userService.createUser(userRequest);
+
+            UserRepresentation userRepresentation = keycloak.realm("ITM").users().search("Beast").get(0);
+
+            Assertions.assertEquals(userRepresentation.getUsername(), userRequest.getUsername());
+            Assertions.assertEquals(userRepresentation.getEmail(), userRequest.getEmail());
+            Assertions.assertEquals(userRepresentation.getFirstName(), userRequest.getFirstName());
+            Assertions.assertEquals(userRepresentation.getLastName(), userRequest.getLastName());
+        }
+
+        @Test
+        @Order(1)
+        void getUserByIdTest() {
+            UserRequest userRequest = createUserRequest();
+            userService.createUser(userRequest);
+
+            String id = keycloak.realm("ITM").users().search("Beast").get(0).getId();
+
+            UserResponse userResponse = userService.getUserById(UUID.fromString(id));
+
+            Assertions.assertEquals(userRequest.getFirstName(), userResponse.getFirstName());
+            Assertions.assertEquals(userRequest.getLastName(), userResponse.getLastName());
+            Assertions.assertEquals(userRequest.getEmail(), userResponse.getEmail());
+        }
+
+        @AfterEach
+        void cleanUp() {
+            UserRepresentation userRepresentation = keycloak.realm("ITM").users().search("Beast").get(0);
+            keycloak.realm("ITM").users().get(userRepresentation.getId()).remove();
+        }
+
+        private UserRequest createUserRequest() {
+            return new UserRequest("vadim", "vadim@email.com", "ujkfjh",
+                    "vadim", "goshin");
+        }
+}
